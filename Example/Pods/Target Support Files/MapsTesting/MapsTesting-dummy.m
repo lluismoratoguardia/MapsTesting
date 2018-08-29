46a89ede3f837eb4725bd951ca6c45402f0e8d87
lluismoratoguardia <lluis.morato@hippo.tech>
2018-08-29 15:14:51 +0200

    Create project pod create lib

---
+++ Example/Pods/Target Support Files/MapsTesting/MapsTesting-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MapsTesting : NSObject
+@end
+@implementation PodsDummy_MapsTesting
+@end
---
+++ Example/Pods/Target Support Files/Pods-MapsTesting_Example/Pods-MapsTesting_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MapsTesting_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MapsTesting_Example
+@end
